d80a944dbddabef0cc5885543e712cfca9b81e46
Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>
2024-08-22 22:14:57 -0300

    [Desafio Implementado]
    - Implementados os métodos necessários para funcionamento do
    programa e adicionadas algumas linhas extras de código para fins
    de robustecimento do sistema.
    
    Signed-off-by: Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,38 +14,58 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+
+            // Implementado e adicionadas algumas validações para robustecer o código 22/08/2024
+            string placaDigitada = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(placaDigitada))
+            {
+                if (!veiculos.Any(x => x.ToUpper() == placaDigitada.ToUpper()))
+                {
+                    // ToUpper para garantir que as letras sejam sempre maiúsculas, independente do input do usuário
+                    veiculos.Add(placaDigitada.ToUpper());
+                }
+                else{
+                    Console.WriteLine("Este veículo já está estacionado aqui. Confira se digitou a placa corretamente!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("O campo de placa deve ser preenchido para estacionar o veículo. Por favor, informe a placa!");
+            }
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            // Implementado e adicionadas algumas validações para robustecer o código 22/08/2024
+
+            // ToUpper para garantir que as letras sejam sempre maiúsculas, independente do input do usuário
+            string placa = Console.ReadLine().ToUpper();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                int horas = Convert.ToInt32(Console.ReadLine());
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                if (horas <= 0)
+                {
+                    Console.WriteLine("O valor digitado para as horas é inválido. Digite um valor maior que zero!");
+                }
+                else
+                {
+                    decimal valorTotal = this.precoInicial + (this.precoPorHora * horas);
+                    veiculos.Remove(placa);
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                }
             }
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente!");
             }
         }
 
@@ -55,8 +75,12 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+
+                // Implementado 22/08/2024
+                foreach (string veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
             }
             else
             {
